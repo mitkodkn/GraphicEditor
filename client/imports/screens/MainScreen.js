e229b77bd41d082550763a78aa38f5f8a730a6a4
dimitar <dimitar@doonloo.com>
2017-12-06 23:56:05 +0200

    Separate app into screens

---
+++ client/imports/screens/MainScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Uploader from "../ui/components/Uploader";
+
+const MainScreen = () => (
+    <span>
+        <Uploader/>
+    </span>
+);
+
+export {MainScreen as default}
--- client/imports/ui/App.js
+++ client/imports/ui/App.js
@@ -1,14 +1,21 @@
 import React from "react";
-import Navigation from "./components/Navigation";
-import Uploader from "./components/Uploader";
+import {BrowserRouter, Switch} from "react-router-dom";
 import {Container} from "semantic-ui-react";
+import {Route} from "react-router";
 
-import 'semantic-ui-css/semantic.min.css';
+import "semantic-ui-css/semantic.min.css";
+
+import Navigation from "./components/Navigation";
+import MainScreen from "../screens/MainScreen";
 
 const App = () => (
     <Container textAlign="center">
         <Navigation/>
-        <Uploader/>
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/" component={MainScreen}/>
+            </Switch>
+        </BrowserRouter>
     </Container>
 );
 
