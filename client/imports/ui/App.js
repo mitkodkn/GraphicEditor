2fbf45d52987035d4591a17230d851978d80be26
dimitar <dimitar@doonloo.com>
2017-12-06 22:43:55 +0200

    Setup React and component rendering

---
+++ client/imports/ui/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Navigation from "./components/Navigation";
+
+const App = () => (
+    <div>
+        <Navigation/>
+    </div>
+);
+
+export {App as default}
---
+++ client/imports/ui/components/Navigation.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react';
+
+export default class Navigation extends Component {
+    render() {
+        return <h1>Navigation</h1>
+    }
+}
