7d984c23714b3072f167401668832d917c92b931
dimitar <dimitar@doonloo.com>
2017-12-07 11:52:24 +0200

    Add complete image loading to editor canvas

--- client/imports/screens/EditorScreen.js
+++ client/imports/screens/EditorScreen.js
@@ -1,19 +1,42 @@
 import React, {Component} from "react";
+import {Redirect} from "react-router-dom";
 import {connect} from "react-redux";
 import {Grid, Segment} from "semantic-ui-react";
 import Slider from "rc-slider";
 
 import "rc-slider/assets/index.css";
+import {calculateAspectRatioFit} from "../Utils";
 
 class EditorScreen extends Component {
+    componentDidMount() {
+        if (this.props.imageFile) {
+            const ctx = this.editorCanvas.getContext('2d');
+
+            this.image = new Image();
+
+            this.image.onload = () => {
+                const {width, height} = calculateAspectRatioFit(this.image.naturalWidth, this.image.naturalHeight, 500, 900);
+                this.editorCanvas.width = width;
+                this.editorCanvas.height = height;
+                ctx.drawImage(this.image, 0, 0, width, height);
+            };
+
+            this.image.src = URL.createObjectURL(this.props.imageFile);
+        }
+    }
+
     render() {
-        console.log(this.props.image);
+        if (!this.props.imageFile) {
+            return <Redirect to="/"/>
+        }
+
+        console.log(this.props.imageFile);
         return (
             <Segment>
                 <Grid columns={2} divided>
                     <Grid.Row>
                         <Grid.Column width={10}>
-
+                            <canvas ref={node => this.editorCanvas = node}/>
                         </Grid.Column>
 
                         <Grid.Column width={6}>
@@ -29,8 +52,8 @@ class EditorScreen extends Component {
     }
 }
 
-const mapStateToProps = state => ({
-    image: state.image,
+const mapStateToProps = ({image}) => ({
+    imageFile: image,
 });
 
 export default connect(mapStateToProps)(EditorScreen);
